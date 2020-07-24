d740d136d2c45b6935505f2ec09a7dfa228828d4
drumovski <jason.stacy@gmail.com>
2020-07-24 10:53:59 +1000

    add edit name seller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -12,9 +12,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    puts "-------------------------------------------------------------here---------------------------------------------"
+    if params[:seller]
+      puts "user is a seller"
+    end
+  end
 
   # GET /resource/edit
   # def edit
@@ -22,9 +26,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    puts "in update"
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -48,9 +53,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :seller])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
