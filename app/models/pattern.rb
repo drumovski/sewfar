d3c4aaafb42e1d64ee4108e2adc6e879700391a7
drumovski <jason.stacy@gmail.com>
2020-07-27 14:30:41 +1000

    change pattern category to enum

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,5 +1,6 @@
 class Pattern < ApplicationRecord
   belongs_to :user
   enum difficulty: {easy: 0, medium: 1, hard: 2}
+  enum category: {women: 0, men: 1, unisex: 2, kids: 3, toys: 4}
   belongs_to :garment
 end
