b12be3f89f0d587d878e17c93413d4c87e17c87f
drumovski <jason.stacy@gmail.com>
2020-08-03 11:01:25 +1000

    fix free pattern not going to stripe

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class PatternsController < ApplicationController
-  before_action :set_pattern, only: [:show, :edit, :update, :destroy]
-  before_action :authorise_change, only: [:edit, :update, :destroy]
-  before_action :authorise_new, only: [:new, :create]
+  before_action :set_pattern, only: %i[show edit update destroy]
+  before_action :authorise_change, only: %i[edit update destroy]
+  before_action :authorise_new, only: %i[new create]
   before_action :authorise_destroy, only: [:destroy]
 
   # GET /patterns
@@ -13,27 +15,29 @@ class PatternsController < ApplicationController
   # GET /patterns/1
   # GET /patterns/1.json
   def show
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      customer_email: current_user.email,
-      line_items: [{
+    if @pattern.price != 0.0
+      session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        customer_email: current_user.email,
+        line_items: [{
           name: @pattern.name,
           # description: @pattern.description,
           amount: (@pattern.price * 100).to_i,
           currency: 'aud',
-          quantity: 1,
-      }],
-      payment_intent_data: {
+          quantity: 1
+        }],
+        payment_intent_data: {
           metadata: {
-              user_id: current_user.id,
-              pattern_id: @pattern.id
+            user_id: current_user.id,
+            pattern_id: @pattern.id
           }
-      },
-      success_url: "#{root_url}payments/success?userId=#{current_user.id}&patternId=#{@pattern.id}",
-      cancel_url: "#{root_url}patterns"
-  )
+        },
+        success_url: "#{root_url}payments/success?userId=#{current_user.id}&patternId=#{@pattern.id}",
+        cancel_url: "#{root_url}patterns"
+      )
 
-  @session_id = session.id
+      @session_id = session.id
+    end
   end
 
   # GET /patterns/new
@@ -42,8 +46,7 @@ class PatternsController < ApplicationController
   end
 
   # GET /patterns/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /patterns
   # POST /patterns.json
@@ -88,31 +91,28 @@ class PatternsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pattern
-      @pattern = Pattern.find(params[:id])
-    end
 
-    def authorise_change
-      if (@pattern.user_id != current_user.id && !current_user.admin) || (!current_user.is_seller && !current_user.admin) 
-        redirect_to patterns_path
-      end
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pattern
+    @pattern = Pattern.find(params[:id])
+  end
 
-    def authorise_destroy
-       if  @pattern.complete && !current_user.admin
-         redirect_to patterns_path
-      end
+  def authorise_change
+    if (@pattern.user_id != current_user.id && !current_user.admin) || (!current_user.is_seller && !current_user.admin)
+      redirect_to patterns_path
     end
+  end
 
-    def authorise_new
-      if !current_user.is_seller
-         redirect_to patterns_path
-      end
-    end
+  def authorise_destroy
+    redirect_to patterns_path if @pattern.complete && !current_user.admin
+  end
 
-    # Only allow a list of trusted parameters through.
-    def pattern_params
-      params.require(:pattern).permit(:name, :sizes, :fabric, :fabric_amount, :garment_id, :category, :price, :description, :difficulty, :notions, :complete, pictures: [], files: [])
-    end
+  def authorise_new
+    redirect_to patterns_path unless current_user.is_seller
+  end
+
+  # Only allow a list of trusted parameters through.
+  def pattern_params
+    params.require(:pattern).permit(:name, :sizes, :fabric, :fabric_amount, :garment_id, :category, :price, :description, :difficulty, :notions, :complete, pictures: [], files: [])
+  end
 end
